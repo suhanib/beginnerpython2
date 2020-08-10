3a07f9c1bcf0b0957fb7789cb67059efc7fcdfc6
ritikav27 <69158131+ritikav27@users.noreply.github.com>
2020-08-10 11:50:58 -0400

    Practice for Class 2

---
+++ Class 2/answers2.py
@@ -0,0 +1,17 @@
+print("hello world")
+age = 14
+print("I am", age)
+print("my last birthday I turned", age)
+
+# interger value
+favoriteNum = 27
+
+
+# float
+pi = 3.14
+
+# boolean
+python = True 
+
+# string
+name = "ritika"
