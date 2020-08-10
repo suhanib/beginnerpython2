f18b370d8dab214f0ca66566300734e24aa88daf
nchikyal <69131696+nchikyal@users.noreply.github.com>
2020-08-10 11:51:05 -0400

    Types of variables

---
+++ Class 2/answers2.py
@@ -0,0 +1,22 @@
+ilike = 10
+print(ilike , "By me")
+print("I am", ilike)
+print("this year i am turning" , ilike)
+print( "Can't you see that I am " , ilike)
+
+# integer values
+
+me = 20637
+grade = 5
+# float 
+
+woo = 10.2
+fractionofcake = 4.233
+  # bolean 
+
+iam248yrsold = False
+me = True
+# string
+
+me = "my name is nishka"
+food = " My favorite food is pizza"
