6bd02c563eea97d647ee3990996f131b7aad3231
wadwad-xhaiden <69084603+wadwad-xhaiden@users.noreply.github.com>
2020-08-10 11:50:11 -0400

    Practice for Class 2

---
+++ Class 2/answers2.py
@@ -0,0 +1,20 @@
+print("hello world")
+print("hello", "world")
+# <-- means a single line comments
+""" 
+three double quote is for 
+multi-line comments
+"""
+age = 13
+print(age)
+# number so no quotes needed
+letter = "a"
+print(letter)
+# a is letter so i need quotes
+print(letter, age)
+print("letter", "age") 
+boolean = True
+floating = 3.14
+integer = 7
+string = "hi"
+print(floating, integer, string, boolean)
