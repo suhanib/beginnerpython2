9d6048adffbbbb44519e21e7477e0f4b607f078e
sugurram14 <69158059+sugurram14@users.noreply.github.com>
2020-08-10 11:51:08 -0400

    Class 2 answers

---
+++ Class 2/answers2.py
@@ -0,0 +1,12 @@
+print("Hello World")
+# Sumati Gurram
+
+age = 12
+
+print(age)
+print("i am ",age)
+print("on my last birthday i turned", age)
+favoritenum = 14
+pi = 3.14
+python = True
+sentence= "Today is my second day of begginer python class. Today I learned how to set up a print command, variables and types of integers."
