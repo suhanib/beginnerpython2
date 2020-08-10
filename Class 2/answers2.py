c0d628b132ba2d665f3e7856c353c09eec320220
reyanshhlinga <69132448+reyanshhlinga@users.noreply.github.com>
2020-08-10 11:50:19 -0400

    Practice for class 2

---
+++ Class 2/answers2.py
@@ -0,0 +1,14 @@
+print("hello world")
+print("10")
+
+# Variables
+age = 10
+print("my age is", age)
+favoriteNum = 9
+grade = 5
+e = 2.718
+pi = 3.14
+python = True
+java = False
+name = "reyanshh"
+sentence ="Hi my name is reyanshh"
