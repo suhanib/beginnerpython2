74db1bb92b187abddbcd9beaa28ea853085b6750
Sara-code-panda <69128072+Sara-code-panda@users.noreply.github.com>
2020-08-10 11:56:46 -0400

    My answers
    Here are my answers!!!!!!!!!!!

---
+++ Class 2/answers2.py
@@ -0,0 +1,16 @@
+print("The Wand chooses the wizard")
+# I love this coding class
+# It is so fun!!!!!!!
+# hi
+age = 10
+print("I am", age )
+Name = ("Sara")
+# integer
+grade = 5
+# float
+pie = 3.14
+#boolean
+java = False
+Python = True
+#string
+sentence = hello my name is sara
