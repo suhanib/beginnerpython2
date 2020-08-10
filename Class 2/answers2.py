8f3ef51f915a5903cee491669072fb43415a1c94
vibhabangalore <vibhabang06@gmail.com>
2020-08-10 11:50:09 -0400

    Practice for Class 2

---
+++ Class 2/answers2.py
@@ -0,0 +1,23 @@
+print("hello world")
+print("13")
+
+# This is a single line comment
+""" 
+This is a 
+multiline comment
+"""
+
+age=13
+print(age)
+print("I am", age)
+print("My age is", age)
+print("people my age are", age)
+
+favoriteNum = 7
+
+pi = 3.14
+
+python = True
+java = False
+
+name = "Vibha Bangalore"
