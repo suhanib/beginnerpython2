66d0e8dcdf40e82e244d7e53edaada1b85541b17
vibhabangalore <vibhabang06@gmail.com>
2020-08-11 16:54:03 -0400

    Homework Answers-Vibha Bangalore

--- Class 2/answers2.py
+++ Class 2/answers2.py
@@ -21,3 +21,11 @@ python = True
 java = False
 
 name = "Vibha Bangalore"
+
+# Homework
+
+""" 
+Question 1: To make a multiline comment you use three quotation marks.
+Question 2: one is a boolean, two is an integer, three is a string, and four is a float
+Question 3: the answer is written below: """
+print("variables are important because they can store information and allow you to easily change it.")
