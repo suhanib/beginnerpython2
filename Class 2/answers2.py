48194dcc78530d46a21571a30ecd252fe547611d
wadwad-xhaiden <69084603+wadwad-xhaiden@users.noreply.github.com>
2020-08-11 11:52:23 -0400

    Homework for Class 2 finished

--- Class 2/answers2.py
+++ Class 2/answers2.py
@@ -1,3 +1,5 @@
+# Practice
+
 print("hello world")
 print("hello", "world")
 # <-- means a single line comments
@@ -18,3 +20,25 @@ floating = 3.14
 integer = 7
 string = "hi"
 print(floating, integer, string, boolean)
+
+# CLASS 2 HOMEWORK
+
+# QUESTION 1: how do you make a multi-line comment?
+"""
+Like this. I am using three double quotes and I can
+write multiple lines as
+long as i close it with another
+"""
+
+# QUESTION 2: what data type is each of these variables?
+one = False
+two = 3
+three = "2.98"
+four = 3.0
+
+""" one is a boolean, two is an integer, three is a string, and four is a float"""
+
+# QUESTION 3: why are variables useful? print out your answer.
+
+print("Variables are useful because if you have to use a certain number or word multiple times in a code,",
+ "rather than changing it in every spot, you can just change the    definition of the variable.")
