fe6f05ab6835112d6ef7a40c8690e8169bd7bdbd
Anika Tokala <anika.tokala@gmail.com>
2020-08-12 17:09:28 +0000

    Homework

---
+++ Class 2/answers2.py
@@ -0,0 +1,6 @@
+"""
+boolean variables
+integer values
+string
+float
+print ( "you don't have to explicitly say what you want you, since python knows the what the variable is based on characteristics of the data type" )
