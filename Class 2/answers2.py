69d78a435aea543371973761d405505140e915aa
sugurram14 <69158059+sugurram14@users.noreply.github.com>
2020-08-13 10:37:34 -0400

    Update answers2.py

--- Class 2/answers2.py
+++ Class 2/answers2.py
@@ -10,3 +10,24 @@ favoritenum = 14
 pi = 3.14
 python = True
 sentence= "Today is my second day of begginer python class. Today I learned how to set up a print command, variables and types of integers."
+
+Question = one 
+
+question Question answer:  "Multiline comments are made by having the three quotation marks on each side of the comment."
+
+Question 2 answer: 
+One is a boolean variable
+Two is a integer value
+Three is a string
+Four is a float
+
+Question 3 answer:
+"""
+Variables are usefull in codeing becuase they can represent numeric values, characters, character strings, or memory 
+addresses. They also give flexability to the coder or programmer. Instead of entering the data directly into the code,
+a coder can use a variable to represent the data.
+"""
+
+Print(Variables are usefull in codeing becuase they can represent numeric values, characters, character strings, or memory 
+addresses. They also give flexability to the coder or programmer. Instead of entering the data directly into the code,
+a coder can use a variable to represent the data.)
