d96db7c373e4230960e6f092792cb5dd99734b3d
deshnamanju <69128537+deshnamanju@users.noreply.github.com>
2020-08-12 21:27:33 -0400

    Update homework2.py

--- Class 2/homework2.py
+++ Class 2/homework2.py
@@ -1,6 +1,6 @@
 # CLASS 2 HOMEWORK
 # put your answers in answers2.py, save the file, commit it to your branch, and then push the changes
-
+Deshna deshnamanju@gmail.com 
 # QUESTION 1: how do you make a multi-line comment?
 
 # QUESTION 2: what data type is each of these variables?
