3b03ad5cf4b6fc800ae16462ba81f07cd7757115
Sara-code-panda <69128072+Sara-code-panda@users.noreply.github.com>
2020-08-13 10:14:35 -0400

    Here is my homework!!!!!

--- Class 2/answers2.py
+++ Class 2/answers2.py
@@ -14,3 +14,6 @@ java = False
 Python = True
 #string
 sentence = hello my name is sara
+# QUESTION 1: you make a multi-line comment with 3 """"
+# QUESTION 2: boolean, integer, string, float 
+print("QUESTION 3: varibles are important because they help save time when coding.")
