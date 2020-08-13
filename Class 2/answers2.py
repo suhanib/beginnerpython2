e0fe37e236dd3271829f31998eec1964b073e011
pratikjha05 <pratikjha05@gmail.com>
2020-08-13 10:37:02 -0400

    Update answers2.py

--- Class 2/answers2.py
+++ Class 2/answers2.py
@@ -1,3 +1,5 @@
+print("Hello World")
+
 age = 14
 grade = 9
 
@@ -8,3 +10,14 @@ banana = True
 sqrt2 = 1.41
 
 print(age, grade, name, banana, sqrt2)
+
+
+
+# Homework 
+
+"""
+1. Multi-line comments can be made by enclosing the comment in triple quotes.
+2. One is a boolean, two is an integer, three is a string, four is a float
+3. Variables are useful because the can be used to represent a value, and the variable can be changed
+easily, changing each place in which the variable was used.
+"""
