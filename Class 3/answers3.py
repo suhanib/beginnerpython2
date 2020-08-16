17d478441bbf9bd69a41eba93d422fd22bc116d9
Anika Tokala <anika.tokala@gmail.com>
2020-08-16 15:14:54 +0000

    Homework

---
+++ Class 3/answers3.py
@@ -0,0 +1,23 @@
+age=12
+if (age<13):
+  print("You cannot go to a Pg-13 movie")
+elif (age>13):
+  print("You can go to a Pg-13 movie")
+
+salary=90000
+years=6
+if (years>5):
+  salary += 4500
+  print(salary)
+
+numberGrade = 23
+if (numberGrade >= 90):
+  print("A")
+elif (numberGrade >= 80):
+  print("B")
+elif (numberGrade >= 70):
+  print("C")
+elif (numberGrade >= 60):
+  print("D")
+elif (numberGrade >= 0):
+  print("F")
