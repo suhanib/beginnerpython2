57a1c61f620fd09815c80e45d3d211ef6fe2f0fa
sugurram14 <69158059+sugurram14@users.noreply.github.com>
2020-08-16 19:46:55 -0400

    Update answers3.py

---
+++ Class 3/answers3.py
@@ -0,0 +1,29 @@
+add = 2 + 2
+substract = 7 - 7
+multiply = 2 * 2
+divide = 15 / 2
+IntegerDivide = 15 // 2
+power = 7 ** 2
+modulo = 14 % 5
+
+num = 2 
+num += 5
+num -= 3
+num *= 3
+num /= 6
+num //= 1
+num **= 2
+num %= 1
+
+age = 12
+if (age >= 13):
+    print("is allowed to watch a PG-13 movie")
+
+havemintchocolatechip = True
+havecookiedough = False
+if (havemintchocolatechip):
+    print("I'll have one scoop of mint chocolatechip ice cream in a cup")
+elif (havecookiedough):
+    print("I'll have one small cookiedough blizzard")
+else:
+    print("I'll have one scoop of vanilla ice cream in a cone") 
