a52ebf3f643e0380cdd23bf8344a60e8232467ab
VedantAwasthi9 <69149096+VedantAwasthi9@users.noreply.github.com>
2020-08-16 20:37:47 -0400

    Work FRom previous class (not homework)

--- Class 2/answers2.py
+++ Class 2/answers2.py
@@ -2,3 +2,25 @@ print("hello world")
 age = 11
 print("People my age are", age)
 print("My age is", age)
+
+# integer value
+Maybe=5
+year=365
+
+# boolean variable
+Maybe=True
+Salad=False
+
+#float
+Maybe=3.14
+Maybe= 2.718
+
+num=3
+num+=5
+
+
+age = 11
+if ("age < 13"):
+    print("I cant watch a pg 13 movie")
+
+elif ("age > 13")
