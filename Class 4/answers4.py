33232258655521d0d408505d1c770a0351e90876
sugurram14 <69158059+sugurram14@users.noreply.github.com>
2020-08-17 12:47:39 -0400

    Update answers4.py

---
+++ Class 4/answers4.py
@@ -0,0 +1,23 @@
+age = 13
+height = 52
+if (age > 12 and height > 50):
+    print("Yes, you can ride the rollercoaster")
+else:
+    print("You cannot ride the rollercoaster")
+
+if (age > 13 or height >= 55):
+    print("Yes, you can ride the rollercoaster")
+else: 
+    print("You cannot ride the rollercoaster")
+
+for i in range(1, 6):
+    print(i)
+
+for i in range (1, 8):
+    print("sumati")
+
+chips = 10
+while (chips > 0):
+    chips -= 1
+    print("ate one chip")
+print("bag is empty")
