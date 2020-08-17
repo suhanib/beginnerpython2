572463daa1bb0cac48763fe5d28906de2845d79d
VedantAwasthi9 <69149096+VedantAwasthi9@users.noreply.github.com>
2020-08-17 10:34:46 -0400

    Homework

---
+++ Class 3/answers3.py
@@ -0,0 +1,15 @@
+#Homework
+timeworked = 6
+if ("timeworked > 5"):
+    print ("your salary is now $94500")
+elif ("timeworked < 5"):
+    print ("no bonus")
+ 
+numberGrade = 77
+
+if ("numberGrade > 90"):
+    print ("A")
+elif ("numberGrade > 80 < 90"):
+    print ("B")
+elif ("numberGrade >70 < 80"):
+    print ("C")
