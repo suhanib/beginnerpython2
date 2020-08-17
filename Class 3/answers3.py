e26b99981faaec100a362d22941acc222301c856
sugurram14 <69158059+sugurram14@users.noreply.github.com>
2020-08-17 09:33:17 -0400

    Update answers3.py

--- Class 3/answers3.py
+++ Class 3/answers3.py
@@ -27,3 +27,28 @@ elif (havecookiedough):
     print("I'll have one small cookiedough blizzard")
 else:
     print("I'll have one scoop of vanilla ice cream in a cone") 
+
+yearsworked = 5
+if(yearsworked <= 5):
+    print("This employee will get a 5 percent bonus on their paycheck")
+
+A = 90-100
+B = 80-89
+C = 70-79
+D = 60-69
+F = 0-59
+numbergrade = 77
+if(numbergrade = 60-69) :
+    print("i got a d on my test")
+if(numbergrade = 70-79) :
+    print("I got a c on my test")
+if(numbergrade = 80-89) :
+    print("I got a b on my test")
+if(numbergrade = 90-100) :
+    print("I got an a on my test")
+if(numbergrade = 0-59) :
+    print("I got a f on my test")
+
+
+
+
