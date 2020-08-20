b98a471369cc8cb26f379489dc26338a8786fcdd
nchikyal <69131696+nchikyal@users.noreply.github.com>
2020-08-20 11:05:04 -0400

    homework 4

---
+++ Class 4/answers4.py
@@ -0,0 +1,61 @@
+Rollerage=14
+rollerHieght=55
+
+if (Rollerage > 12 and rollerHieght> 50):
+    print("You will be able to go on this roller coaster.")
+else:
+    print("You cannot go on this ride")
+
+coasterAge=14
+Coasterhieght=60
+
+if (coasterAge >= 13 or Coasterhieght>= 55):
+    print( "you can go on the roller coaster")
+else:
+    print("You cannot go on this ride")
+
+
+#loops
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+
+
+for i in range (1, 8):
+    print("Nishka")
+
+chips=10
+
+while (chips>0):
+    chips-= 9
+    print("ate one chip")
+
+#Homework
+#1
+num=7
+total=0
+
+for i in range(1,num+1):
+    total+=i
+print()
+
+#2
+for i in range(1500,2701):
+    if (i%5==0 and i % 7 ==0):
+        print(i)
+#3
+x = 10
+while (x>0):
+    print(x)
+    x-=1
+print("liftoff!!")
+
+
+
+
+
+
+  
+
