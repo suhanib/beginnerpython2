6527bee3d1e53897a649cce52b9fbdc4650f5549
wadwad-xhaiden <69084603+wadwad-xhaiden@users.noreply.github.com>
2020-08-20 12:02:53 -0400

    Class 5

---
+++ Class 5/answers5.py
@@ -0,0 +1,23 @@
+name = "Bobby"
+
+print("Hi there " + name + ". How are you?")
+
+hi = "Hi there " + name + ". How are you?"
+
+print(hi)
+
+print("mew " * 12)
+
+num = "3"
+
+print(int(num))
+print(float(num))
+
+mun = "13.75"
+print(int(float(mun)))
+
+name = input("What is your name\n")
+if (name[0].islower()):
+    print("Capatilize the first letter")
+else:
+    print("Good job")
