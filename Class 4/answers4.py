7a9942f7434131449cbd17b889957f9fb4a9040e
sugurram14 <69158059+sugurram14@users.noreply.github.com>
2020-08-20 11:03:13 -0400

    Update answers4.py

--- Class 4/answers4.py
+++ Class 4/answers4.py
@@ -21,3 +21,19 @@ while (chips > 0):
     chips -= 1
     print("ate one chip")
 print("bag is empty")
+
+num = 10
+total = 0
+for i in range(1,num + 1):
+    total += i
+
+print(total)
+for i in range(1500, 2701):
+    if (i % 5 == 0 and i % 7 == 0):
+        print(i)
+
+x = 10
+while (x > 0):
+    print(x)
+    x-=1
+print("liftoff!")
