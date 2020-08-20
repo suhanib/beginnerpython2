24eaed18adc7d839dc5deb270ec6b307156a247e
pratikjha05 <pratikjha05@gmail.com>
2020-08-20 10:12:12 -0400

    class 4

---
+++ Class 4/answers4.py
@@ -0,0 +1,23 @@
+height = 55
+age = 12
+
+if(age > 12 and height > 50):
+    print("You can ride rollercoaster #1!")
+if(age>= 13 or height>=55):
+    print("You can ride rollercoaster #2!")
+
+
+if("ti" in "pratik"):
+    print("what's the point of this again?")
+
+for i in range(1, 12):
+    print(i)
+print("end")
+
+for i in range(1, 8):
+    print("Pratik")
+
+chips = 10
+while (chips >0):
+    chips -= 1
+    print("You ate one chip, you now have", chips, "chips")
