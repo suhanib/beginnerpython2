5f4e5db444a714bbaf090ac94beef8064f08c3d5
VedantAwasthi9 <69149096+VedantAwasthi9@users.noreply.github.com>
2020-08-20 11:02:15 -0400

    Homework After Help

--- Class 4/answers4.py
+++ Class 4/answers4.py
@@ -30,14 +30,18 @@ while (x > 0):
 print("liftoff!")
 
 
-#Question 1 (Number chosen 3)
-
-#?
+#Question 1 (Number chosen 8)
+num = 8
+total = 0
+for i in range(1,num + 1):
+    total += i 
+print(total)
 
 #Question 2 
 
-#?
-
+for i in range(1500, 2701):
+    if (i % 5 ==  i % 7 == 0):
+        print(i)
 
 
 
