9157d7c3d94d84f2c1cae3cf20cc14a1b41f8a1d
sugurram14 <69158059+sugurram14@users.noreply.github.com>
2020-08-24 08:37:22 -0400

    Update answers5.py

---
+++ Class 5/answers5.py
@@ -0,0 +1,9 @@
+word = "fun"
+print("going to the waterpark is" + word)
+
+activity = ("going to the waterpark is" + word)
+print(activity)
+
+example= 10.5
+print(int(example))
+print(float(example))
