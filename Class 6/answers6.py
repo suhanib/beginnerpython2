5c7648facc979b1d19cbe4720af2d866df3042fd
Sara-code-panda <69128072+Sara-code-panda@users.noreply.github.com>
2020-08-24 12:15:23 -0400

    Here is my practice during class!!

---
+++ Class 6/answers6.py
@@ -0,0 +1,16 @@
+#Try it on your own
+grocerylist = ["eggs", "apples", "milk", "choclate", "instant coffee", "plastic cups", "chips"]
+print(grocerylist[0])
+print(grocerylist[1])
+print(grocerylist[2])
+print(grocerylist[3])
+print(grocerylist[4])
+print(grocerylist[4:6])
+grocerylist[6] = "dried chili"
+print(grocerylist)
+grocerylist[4] = "dog food"
+print(grocerylist)
+grocerylist.append("ice cream")
+print(grocerylist)
+grocerylist.remove("apples")
+print(grocerylist)
