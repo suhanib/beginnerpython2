1f7aa9e6ee42d238e1b128b8836d99f9813d9801
VedantAwasthi9 <69149096+VedantAwasthi9@users.noreply.github.com>
2020-08-23 22:56:17 -0400

    Homework (Need more practice)

---
+++ Class 5/answers5.py
@@ -0,0 +1,19 @@
+E = "hose"
+
+print("With the "  + E +  " we watered the plants")
+
+S = "Using the" + E + "I watered the plants"
+
+Word = "Cheese"
+
+print("i like " + Word * 3 + "")
+example=13
+print(int(example))
+print(float(example))
+
+#Homework
+print("my favorite number is 343 bc it is 7 squared. i also like 64.")
+
+#? Word = summer
+
+#? Strings are hard, I need more practice with them
