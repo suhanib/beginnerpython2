39f7b26e1d3729a7f79c093eb029c12b0f4c31c0
SomeoneNamedSomeone <69155864+SomeoneNamedSomeone@users.noreply.github.com>
2020-08-24 10:36:09 -0400

    Update answers5.py

---
+++ Class 5/answers5.py
@@ -0,0 +1,12 @@
+#1.
+
+num1 = 343
+num2 = int(7.0)
+num3 = 8*8
+
+print("my favorite number is " + str(num1) + " bc it is " + str(num2) + " squared. i also like " + str(num3))
+
+#2.
+
+test = "aasaAauAaamaAmaAAaaeaaAAAraaaa"
+print(test.strip("a"))
