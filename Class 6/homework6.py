a8b47a843b63ce071d7677e374aa7c5d1dede50f
suhanib <38591664+suhanib@users.noreply.github.com>
2020-08-24 12:07:57 -0400

    class 6
    homework 5 answers
    notes 6
    homework 6 questions

---
+++ Class 6/homework6.py
@@ -0,0 +1,17 @@
+# CLASS 6 HOMEWORK
+# put your answers in answers6.py, save the file, commit it to your branch, and then push the changes
+# i'll be checking and making comments on your homework, so make sure you get it done! :)
+
+# HINT: loops are very helpful to answer all of these questions!!
+
+# QUESTION 1: make a list and print out every item in a list one at a time. do not do print(listName).
+listOne = ["i", "am", "a", "sentence"]
+
+# QUESTION 2: write a program to sum all items in a list
+listTwo = [4,3,6,10,32,86]
+
+# QUESTION 3: convert a string into an actual list, where each element is one character
+string = "helloiamastring"
+
+# QUESTION 4: go through this list and make a new output list that only contains words from the first list that start with "a". 
+listThree = ["apple", "hello", "awesome", "ew", "bad", "nice", "ant"]
