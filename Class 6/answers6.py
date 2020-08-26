5201cc408750b502c2d2f46f6733cf604911cfea
sugurram14 <69158059+sugurram14@users.noreply.github.com>
2020-08-26 17:42:26 -0400

    Update answers6.py

--- Class 6/answers6.py
+++ Class 6/answers6.py
@@ -14,3 +14,27 @@ print(listOne[3])
 print(listOne[4])
 
 # Question 2 answer
+listTwo = [4,3,6,10,32,86]
+# I dont understand
+
+# Question 3 answer
+string = "helloiamastring"
+print(string[0])
+print(string[1])
+print(string[2])
+print(string[3])
+print(string[4])
+print(string[5])
+print(string[6])
+print(string[7])
+print(string[8])
+print(string[9])
+print(string[10])
+print(string[11])
+print(string[12])
+print(string[13])
+print(string[14])
+
+# Question 3 answer
+listThree = ["apple", "hello", "awesome", "ew", "bad", "nice", "ant"]
+# dont understand
