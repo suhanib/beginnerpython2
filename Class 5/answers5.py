cbbe3985698e3cf3ba3145b80e7fa194bec113bb
sugurram14 <69158059+sugurram14@users.noreply.github.com>
2020-08-26 17:31:15 -0400

    Update answers5.py

--- Class 5/answers5.py
+++ Class 5/answers5.py
@@ -1,14 +1,25 @@
 word = "fun"
-print("going to the waterpark is" + word)
+print("going to the waterpark is " + word)
 
-activity = ("going to the waterpark is" + word)
+activity = ("going to the waterpark is " + word)
 print(activity)
 
 example= 10.5
 print(int(example))
 print(float(example))
 
-print("my favorite number is 343 bc it is 7 squared, I also like 64.")
+print("my favorite number is "+ str(343)  + " bc it is " + str(int(7.0)) + " cubed I also like " + str(8*8))
 
 string = "aasaAauAaamaAmaAAaaeaaAAAraaa"
-string1 = string replace ("a", "")
+string = string.lower()
+string = string.replace("a", "")
+print(string)
+
+password = "12345"
+
+if (password.isalpha() or password.isdigit()):
+    print("not secure")
+elif (password.islower()):
+    print("not secure")
+else:
+    print("secure password")
