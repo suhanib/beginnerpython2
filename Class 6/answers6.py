785dfe96dd26ecb5ed97b954fa973d83015a2585
sugurram14 <69158059+sugurram14@users.noreply.github.com>
2020-08-26 17:31:19 -0400

    Update answers6.py

---
+++ Class 6/answers6.py
@@ -0,0 +1,8 @@
+summerbucketlist = ["go to the beach", "have a sleepover", "go shopping", "go to the pool", "eat ice cream"]
+print(summerbucketlist[4])
+print(summerbucketlist[0:3])
+
+summerbucketlist.append("hangout with friends")
+summerbucketlist.append("watch movies")
+print(summerbucketlist)
+
