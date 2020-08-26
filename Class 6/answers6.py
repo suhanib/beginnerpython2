2aabe3846f959d63d357c550fc3f712f3bc3c9cf
sugurram14 <69158059+sugurram14@users.noreply.github.com>
2020-08-26 17:34:23 -0400

    Update answers6.py

--- Class 6/answers6.py
+++ Class 6/answers6.py
@@ -6,3 +6,11 @@ summerbucketlist.append("hangout with friends")
 summerbucketlist.append("watch movies")
 print(summerbucketlist)
 
+# Question 1 answer
+listOne = ["i", "am", "a", "sentence"]
+print(listOne[1])
+print(listOne[2])
+print(listOne[3])
+print(listOne[4])
+
+# Question 2 answer
