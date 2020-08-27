9fee41f476fd14719b58d4e8ad3b59d776242516
VedantAwasthi9 <69149096+VedantAwasthi9@users.noreply.github.com>
2020-08-27 10:00:58 -0400

    Homework

--- Class 6/answers6.py
+++ Class 6/answers6.py
@@ -7,7 +7,21 @@ print(groceryList)
 groceryList.insert(2, "Berries")
 print(groceryList)
 
-del groceryList[2]
-print(groceryList)
+#homework 
+listOne = ["i", "am", "a", "sentence"]
+print(listOne)[1:5]
 
-groceryList.remove(1)
+#2
+listTwo = [4,3,6,10,32,86]
+total = sum(listTwo)
+print(total)
+#?
+
+
+#3
+listThree = ["Hello", "I", "Am", "a", "string"]
+print(listThree)[1:5]
+
+#4 
+
+mylist = ["Apple", "Awesome", "Ant", "Am", "a"]
