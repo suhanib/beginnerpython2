05ad94ed66785239189341fdf6ca6564a5d5b82d
deshnamanju <69128537+deshnamanju@users.noreply.github.com>
2020-08-27 10:01:49 -0400

    Update homework6.py

--- Class 6/homework6.py
+++ Class 6/homework6.py
@@ -6,12 +6,13 @@
 
 # QUESTION 1: make a list and print out every item in a list one at a time. do not do print(listName).
 listOne = ["i", "am", "a", "sentence"]
-
+(Travellist) = ("toothbrush", "suitcases", "passport", "cloths","sanitary needs")
 # QUESTION 2: write a program to sum all items in a list
 listTwo = [4,3,6,10,32,86]
-
+(howpeopleinapartylist)=(1,2,3,4,5)
 # QUESTION 3: convert a string into an actual list, where each element is one character
 string = "helloiamastring"
 
 # QUESTION 4: go through this list and make a new output list that only contains words from the first list that start with "a". 
 listThree = ["apple", "hello", "awesome", "ew", "bad", "nice", "ant"]
+(alist)=("apple pie","art","ant","article","acting")
