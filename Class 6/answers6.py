8d7bedf5ec6a2dff95e057524abee4e0107ddd4b
deshnamanju <69128537+deshnamanju@users.noreply.github.com>
2020-08-26 20:28:12 -0400

    Update answers6.py

---
+++ Class 6/answers6.py
@@ -0,0 +1,13 @@
+groceryList = ("watermelones", "peaches", "lemons", "cilantro", "mint", "onions")
+print(groceryList)
+print (groceryList(4))
+print(groceryList(0:3))
+
+groceryList(4) = "cherries"
+
+
+groceryList.append("bananas")
+groceryList.append("cheese")
+print(groceryList)
+
+listname.remove("lemons")
