20bcf11d4ea6d65f4c74105280f32896cd036124
VedantAwasthi9 <69149096+VedantAwasthi9@users.noreply.github.com>
2020-08-27 10:01:52 -0400

    Homework

---
+++ Class 6/answers6.py
@@ -0,0 +1,18 @@
+#homework 
+listOne = ["i", "am", "a", "sentence"]
+print(listOne)[1:5]
+
+#2
+listTwo = [4,3,6,10,32,86]
+total = sum(listTwo)
+print(total)
+#?
+
+
+#3
+listThree = ["Hello", "I", "Am", "a", "string"]
+print(listThree)[1:5]
+
+#4 
+
+mylist = ["Apple", "Awesome", "Ant", "Am", "a"]
