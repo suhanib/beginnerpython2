455ba7f2da6c9c0d15f03eeb66eb2e824a766744
nchikyal <69131696+nchikyal@users.noreply.github.com>
2020-08-26 23:11:21 -0400

    homework

---
+++ Class 6/answers6.py
@@ -0,0 +1,33 @@
+#lists
+schoolList=["notebook", "pencils", "Markers","Pencil case", "Binder", "IDR book", "Calculator"]
+print(schoolList)
+
+print(schoolList[4])
+print(schoolList[4:7])
+
+#homework
+#1
+listOne = ["i", "am", "a", "sentence"]
+
+for x in  listOne:
+    print(x)
+
+#2
+
+listTwo = [4,3,6,10,32,86]
+total = 0
+for i in listTwo:
+    total = total + i
+     
+print(total)
+
+#3
+
+listThree= ["hello" , "i", "am", "a", "string"]
+
+print(listThree[0:5])
+
+#4
+
+listFour = ["apple", "hello", "awesome", "ew", "bad", "nice", "ant"]
+
