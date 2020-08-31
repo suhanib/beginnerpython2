f1b36d18bd9d6036a62e3bf457595d9a6090bcc9
sugurram14 <69158059+sugurram14@users.noreply.github.com>
2020-08-31 11:57:23 -0400

    Update answers8.py

---
+++ Class 8/answers8.py
@@ -0,0 +1,37 @@
+listOne = ["start", "middle", "end"]
+for i in range(0, 5):
+    for item in listOne:
+        print(item)
+
+listTwo = ["start2","middle2", "end2"]
+listThree = ["start3", "middle3", "end3"]
+listFour = ["start4", "middle4", "end4"]
+listFive = ["start5", "middle5", "end5"]
+
+for i in range(0, 5):
+    for item in listTwo:
+        print(item)
+
+for i in range(0, 5):
+    for item in listThree:
+        print(item)
+
+for i in range(0, 5):
+    for item in listFour:
+        print(item)
+
+for i in range(0, 5):
+    for item in listFive:
+        print(item)
+
+def example(param):
+    print(param)
+    return True
+example("hello") 
+
+def add(a,b):
+    return a + b
+
+print(add(7,8))
+print(add(9,7)) 
+print(add(276, 987))   
