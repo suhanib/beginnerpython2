63dcccc4ba534b66aa1aab6e37a02220354459b4
YOLO101 <69153710+YOLO101@users.noreply.github.com>
2020-08-31 13:30:41 -0400

    Create notes3.py

---
+++ Class 3/notes3.py
@@ -0,0 +1,22 @@
+add=7+7
+Subtract=7-7
+multiply=7*4
+divide=7/7
+intgerDivide= 15//2
+" One slash will is called float divison. It will give you a decimal ever time. 15/2=7.5. An interger divison isn't going to round your answer. It will just chop off the decimal. 15//2=7"
+power=7**2
+modulo=14%5 #return 4 because the remainder
+" "
+add+=4
+Subtract-=3
+divide/=2
+intgerDivide //=2
+' '
+Add==1+1
+subtract=1-1
+Add+=2
+subtract-=0
+print(Add)
+Age=12
+if (num>13)
+print (I can not watch a PG-13 movie)
