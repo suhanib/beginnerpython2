49724e34c7dc1b6b587dc165212138580f36f02b
sugurram14 <69158059+sugurram14@users.noreply.github.com>
2020-09-03 16:05:44 -0400

    Update answers8.py

--- Class 8/answers8.py
+++ Class 8/answers8.py
@@ -37,3 +37,10 @@ print(add(9,7))
 print(add(276, 987)) 
 
 # Question 1 answer
+total = 0
+list1 = [11, 5, 17, 18, 23] 
+for ele in range(0, len(list1)): 
+    total = total + list1[ele]
+print("Sum of all elements in given list: ", total) 
+
+# Question 2 answer
