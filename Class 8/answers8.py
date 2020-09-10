ab346011ddb55f800c64f54822277e77d26beca7
VedantAwasthi9 <69149096+VedantAwasthi9@users.noreply.github.com>
2020-09-10 16:49:00 -0400

    Update answers8.py

--- Class 8/answers8.py
+++ Class 8/answers8.py
@@ -2,3 +2,21 @@ def add(num,num2):
     return num + num2
        
 print(add(5,5))
+
+
+
+def vowelCount(string):
+    count = 0
+    for char in string:
+        if (char == "a"):
+            count += 1
+            if (char == "e"):
+                count += 1
+            elif (char == "i"):
+                count += 1
+            elif (char == "o"):
+                count += 1
+            elif (char == "u"):
+                count += 1
+        return count
+    print(vowelCount("awegitoxuq"))
