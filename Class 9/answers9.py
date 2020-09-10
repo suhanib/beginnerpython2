f47d399cdb3ab3c73b15e1bb3e8d1a5bb80105e3
sugurram14 <69158059+sugurram14@users.noreply.github.com>
2020-09-10 15:24:05 -0400

    Update answers9.py

---
+++ Class 9/answers9.py
@@ -0,0 +1,4 @@
+number = input("What's your class number")
+print("mine to 2 times", number)
+mynumber = int(input("how many days till your birthday"))
+print("my birthday is coming in ", mynumber + 7)
